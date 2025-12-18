83903c6d228b4ff907376c0d7c8dd942b1f5d0af
CoffeeSi <ewgenik02032006@gmail.com>
2025-12-18 20:36:52 +0500

    feat(shapes): shape initialization in the main package

--- assignment1/main.go
+++ assignment1/main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
-	"github.com/CoffeeSi/golangAITU/assignment1/Library"
+	"fmt"
+
+	"github.com/CoffeeSi/golangAITU/assignment1/Shapes"
 )
 
 func main() {
-	library := new(Library.Library)
-	library.ConsoleMenu()
+	// library := new(Library.Library)
+	// library.ConsoleMenu()
+
+	rectangle := Shapes.Rectangle{
+		A: 4,
+		B: 6,
+	}
+	circle := Shapes.Circle{
+		R: 4,
+	}
+	square := Shapes.Square{
+		A: 4,
+	}
+	triangle := Shapes.Triangle{
+		A: 3,
+		B: 4,
+		C: 5,
+	}
+
+	shapes := []Shapes.Shape{rectangle, circle, square, triangle}
+
+	for _, shape := range shapes {
+		fmt.Printf("Area = %v; Perimetr = %v\n", shape.CalculateArea(), shape.CalculatePerimetr())
+	}
 }
