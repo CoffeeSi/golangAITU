e65a64c03f3ef88d367883757a0850fa52019e40
CoffeeSi <ewgenik02032006@gmail.com>
2025-12-19 01:15:46 +0500

    feat(main): refactoring main module

--- assignment1/Library/library.go
+++ assignment1/Library/library.go
@@ -54,17 +54,20 @@ func (library *Library) ListAvailableBooks() {
 		return
 	}
 	for i, book := range library.Books {
-		fmt.Printf("%v. ID: %v, Title: %s, Author: %s, IsBorrowed: %t\n", i, book.ID, book.Title, book.Author, book.IsBorrowed)
+		fmt.Printf("%v. ID: %v, Title: %s, Author: %s, IsBorrowed: %t\n", i+1, book.ID, book.Title, book.Author, book.IsBorrowed)
 	}
 }
 
 func (library *Library) ConsoleMenu() {
 	var scanner = bufio.NewScanner(os.Stdin)
+	scanner.Scan()
 	var id uint64 = 0
-	fmt.Print("Input operation:\n 1. Add book\n 2. Borrow book\n 3. Return book \n 4. List available books \n 0. Exit \n")
-	var operation string
-	for scanner.Scan() {
-		operation = scanner.Text()
+	for {
+		fmt.Print("\nInput operation:\n 1. Add book\n 2. Borrow book\n 3. Return book \n 4. List available books \n 0. Exit\n: ")
+		if !scanner.Scan() {
+			return
+		}
+		operation := scanner.Text()
 		switch operation {
 		case "1":
 			var title, author string
@@ -87,7 +90,11 @@ func (library *Library) ConsoleMenu() {
 			fmt.Print("ID of book to borrow: ")
 			scanner.Scan()
 			bookId, _ = strconv.ParseUint(scanner.Text(), 10, 64)
-			book := library.Books[bookId]
+			book, exists := library.Books[bookId]
+			if !exists {
+				fmt.Println("Book does not exist")
+				continue
+			}
 			library.BorrowBook(book)
 		case "3":
 			var bookId uint64
--- assignment1/main.go
+++ assignment1/main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"fmt"
 
+	"github.com/CoffeeSi/golangAITU/assignment1/Bank"
+	"github.com/CoffeeSi/golangAITU/assignment1/Company"
 	"github.com/CoffeeSi/golangAITU/assignment1/Library"
 	"github.com/CoffeeSi/golangAITU/assignment1/Shapes"
-  "github.com/CoffeeSi/golangAITU/assignment1/Company"
-  "github.com/CoffeeSi/golangAITU/assignment1/Bank"
-  
 )
 
-func main() {
-// 	library := new(Library.Library)
-// 	library.ConsoleMenu()
+func UseLibrarySystem() {
+	library := new(Library.Library)
+	library.ConsoleMenu()
+}
 
+func UseShapes() {
 	rectangle := Shapes.Rectangle{
 		A: 4,
 		B: 6,
@@ -35,33 +36,64 @@ func main() {
 	for _, shape := range shapes {
 		fmt.Printf("Area = %v; Perimetr = %v\n", shape.CalculateArea(), shape.CalculatePerimetr())
 	}
-  
-  employee := Company.PartTimeEmployee{
+}
+
+func UseCompanySystem() {
+	company := Company.Company{}
+	employeeYevgeniy := Company.PartTimeEmployee{
 		ID:        1,
 		FirstName: "Yevgeniy",
 		LastName:  "Averyanov",
-		Position:  "Middle",
-		Salary:    500000,
+		Position:  "Junior",
+		Salary:    300000,
 		Shifts:    3,
 	}
-
-	employee2 := Company.FullTimeEmployee{
-		ID:        0,
+	employeeArlan := Company.FullTimeEmployee{
+		ID:        2,
 		FirstName: "Arlan",
 		LastName:  "Tursyn",
-		Position:  "Senior",
-		Salary:    1800000,
+		Position:  "Middle",
+		Salary:    900000,
 	}
-	google := Company.Company{}
-	google.AddEmployee(employee)
-	google.AddEmployee(employee2)
-	google.ListEmployees()
-  
-  account := Bank.BankAccount{
+
+	company.AddEmployee(employeeYevgeniy)
+	company.AddEmployee(employeeArlan)
+	company.ListEmployees()
+}
+
+func UseBankSystem() {
+	account := Bank.BankAccount{
 		Name:          "Yevgeniy",
 		AccountNumber: 12345678,
 		Money:         0,
 		Transactions:  []string{},
 	}
 	account.ConsoleMenu()
+}
+
+func main() {
+	fmt.Println("- Choose exercise:")
+	fmt.Println(" 1. Library Management System")
+	fmt.Println(" 2. Shapes & Interfaces")
+	fmt.Println(" 3. Employee Management System")
+	fmt.Println(" 4. Bank Account Simulation")
+	fmt.Println(" 0. Exit")
+	fmt.Print(": ")
+	var exercise int
+	fmt.Scan(&exercise)
+	switch exercise {
+	case 1:
+		UseLibrarySystem()
+	case 2:
+		UseShapes()
+	case 3:
+		UseCompanySystem()
+	case 4:
+		UseBankSystem()
+	case 0:
+		return
+	default:
+		fmt.Println("Incorrect exercise!")
+	}
+
 }
