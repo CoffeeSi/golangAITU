53abeeb9670a58702bd6388710bbbc1062824ca6
CoffeeSi <ewgenik02032006@gmail.com>
2025-12-18 21:48:53 +0500

    feat(company): employee management system implementation

--- assignment1/main.go
+++ assignment1/main.go
@@ -1,1 +1,26 @@
 package main
+
+import "github.com/CoffeeSi/golangAITU/assignment1/Company"
+
+func main() {
+	employee := Company.PartTimeEmployee{
+		ID:        1,
+		FirstName: "Yevgeniy",
+		LastName:  "Averyanov",
+		Position:  "Middle",
+		Salary:    500000,
+		Shifts:    3,
+	}
+
+	employee2 := Company.FullTimeEmployee{
+		ID:        0,
+		FirstName: "Arlan",
+		LastName:  "Tursyn",
+		Position:  "Senior",
+		Salary:    1800000,
+	}
+	google := Company.Company{}
+	google.AddEmployee(employee)
+	google.AddEmployee(employee2)
+	google.ListEmployees()
+}
